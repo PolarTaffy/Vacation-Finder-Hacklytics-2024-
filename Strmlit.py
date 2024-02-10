35d91ebbb5eb383cab5d7a1aaf0d83c6e7b292c0
Evan Thomas <thomasevan1248@gmail.com>
2024-02-10 13:37:45 -0500

    Streamlit Input Test

--- Strmlit.py
+++ Strmlit.py
@@ -1,3 +1,18 @@
 import streamlit as st
 
+temp = True;
+
+st.text("Welcome to my app!")
+
 st.write("Hello World")
+
+temp
+
+numPeople = st.number_input("Number of People", min_value=0)
+
+
+activities = st.text_input('List Some Activities to Do')
+
+
+
+st.write('Currently, ', numPeople, ' people are attending')
