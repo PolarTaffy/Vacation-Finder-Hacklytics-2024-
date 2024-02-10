15c723ec14b39abd11208262414dad74ad88a201
Evan Thomas <thomasevan1248@gmail.com>
2024-02-10 14:49:41 -0500

    Implemented Buttons + 2 Qs

--- Strmlit.py
+++ Strmlit.py
@@ -1,18 +1,37 @@
 import streamlit as st
+prompt = ""
 
-temp = True;
+st.title("Tour Around the World!")
+st.header("Let's talk about your trip details.")
 
-st.text("Welcome to my app!")
-
-st.write("Hello World")
-
-temp
-
-numPeople = st.number_input("Number of People", min_value=0)
-
-
-activities = st.text_input('List Some Activities to Do')
+numPeople = st.number_input("How many people are coming?", min_value=1)
+budget = st.slider("What's your budget?", 100, 10000)
 
 
 
-st.write('Currently, ', numPeople, ' people are attending')
+
+
+# methods
+def returnOutput(prompt):
+    
+    prompt = "Plan an affordable vacation on a $" + str(budget) + ' for ' + str(numPeople) + ' people.'
+    aiResponse = "Not yet implemented"
+
+    st.header("Prompt (Debug Purposes)")
+    st.write(prompt)
+
+    st.header("AI Response")
+    st.write('Generating...')
+    st.write(aiResponse)
+
+
+st.divider()
+if st.button("Plan my vacation!"):
+    returnOutput("")
+
+# feed the promp into
+
+
+
+
+
